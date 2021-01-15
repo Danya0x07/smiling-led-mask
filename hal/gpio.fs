96bbbe5b191aae419f5847d3cf008be165865024
Danya0x07 <dlef0xf8@gmail.com>
2021-01-15 11:44:42 +0300

    Адаптация к капризам терминальной программы.

--- hal/gpio.fs
+++ hal/gpio.fs
@@ -11,28 +11,23 @@ $2b constant PORTD
 \ Например, чтобы объявить пин 7 порта D:
 \   PORTD 7 defpin PD7
 : defpin: ( PORTx #pin --- <word> | <word> --- mask port)
-    1 swap lshift  \ Преобразовать номер пина в битовую маску.
+    1 swap lshift  \ Convert pin number to bitmask.
     create
-        c, c,  \ Запоминаем адрес порта и битовую маску пина.
+        c, c,  \ Save address and bitmask.
     does>
-        dup c@      \ Достаём битовую маску пина.
-        swap 1+ c@  \ Достаём порт.
-;
+        dup c@      \ Push bitmask.
+        swap 1+ c@  \ Push address.
+    ;
 
 \ Установка пина в режим выхода.
 : mode-output ( pinmask portaddr -- )
-    \ Воспользуемся тем, что для любого x соблюдается равенство адресов
     \ PORTx == DDRx + 1 == PINx + 2
-    1-
-    mset
-;
+    1- mset ;
 
 \ Установить высокий лог. уровень на пине.
 : high ( pinmask portaddr -- )
-    mset
-; inlined
+    mset ; inlined
 
 \ Установить низкий лог. уровень на пине.
 : low ( pinmask portaddr -- )
-    mclr
-; inlined
+    mclr ; inlined
