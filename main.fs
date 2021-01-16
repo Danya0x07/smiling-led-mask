8d4f0ad10375e3324584014bb63fb7d7e89f7090
Danya0x07 <dlef0xf8@gmail.com>
2021-01-16 21:42:02 +0300

    Добавлен фильтр значений громкости.

--- main.fs
+++ main.fs
@@ -11,8 +11,8 @@
 marker -main
 
 decimal
-5 constant num-leds
-2 constant filter-length
+5 constant num-pins  \ Кол-во пинов, к которым подключены светодиоды.
+2 constant fb-length  \ Длина фильтровочного буфера.
 
 PORTB 5 defpin: PB5
 PORTB 2 defpin: PB2
@@ -21,12 +21,30 @@ PORTB 0 defpin: PB0
 PORTD 7 defpin: PD7
 
 create led-pins ' PB5 , ' PB2 , ' PB1 , ' PB0 , ' PD7 ,
-create thresholds 20 , 100 , 240 , 360 , 400 ,
+create thresholds 20 , 100 , 240 , 360 , 400 ,  \ Громкостные пороги
 
+create filter-buff fb-length cells allot
+variable idx
+
+\ Фильтр "скользящее среднее"
+: filter ( volume -- filtered )
+    filter-buff  idx @ cells +  !
+    idx @ 1+ 
+    dup fb-length = if
+        drop 0 then
+    idx !
+    0 
+    fb-length for
+        filter-buff r@ cells + @
+        +
+        next
+    fb-length / ;
+
+\ Зажечь улыбку в соответствии с громкостью.
 : smile ( volume -- )
-    num-leds for
+    num-pins for
         dup
-        num-leds r@ - 1-  \ get classic loop index 0 to num-leds - 1
+        num-pins r@ - 1-  \ get classic loop index 0 to num-pins - 1
         dup >r
         cells thresholds + @  \ volume, threshold
         >  \ flag
@@ -36,3 +54,5 @@ create thresholds 20 , 100 , 240 , 360 , 400 ,
         else low then
     next 
     drop ;
+
+
